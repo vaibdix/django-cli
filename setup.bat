1bd2ead5aa3ba452dbfd98b2da9d278b129f9837
vaibdix <dixitvaib@gmail.com>
2025-05-03 14:09:53 +0530

    Add setup script for installing Django CLI to user profile

---
+++ setup.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Installing Django CLI...
+copy django-cli.exe %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\
+echo Django CLI installed! You can now use it from anywhere by typing 'django-cli'
+pause
